11746c2f8770e260346a41493727cafc0e377909
Luciano Abbott <luciano.abbott@gmail.com>
2018-06-05 11:08:42 -0400

    LA: Debugging referencing problem

--- k8.ts
+++ k8.ts
@@ -1,15 +1,14 @@
-
-//% weight=100 color=#421C52 icon=""
+//% weight=100 color=#421C52 icon="\uf1b9"
 namespace k8 {
-  let IR_SENSOR_0 = AnalogPin.P0
-  let IR_SENSOR_1 = AnalogPin.P1
-  let SPEAKER = AnalogPin.P1
-  let IR_SENSOR_2 = AnalogPin.P2
-  let SERVO_2 = AnalogPin.P8
-  let SONAR = DigitalPin.P8
-  let SERVO_1 = AnalogPin.P12
-  let M2_PWR = AnalogPin.P13
-  let M2_DIR = AnalogPin.P14
-  let M1_PWR = AnalogPin.P15
-  let M2_DIR = AnalogPin.P16
+  export let IR_SENSOR_0 = AnalogPin.P0
+  export let IR_SENSOR_1 = AnalogPin.P1
+  export let SPEAKER = AnalogPin.P1
+  export let IR_SENSOR_2 = AnalogPin.P2
+  export let SERVO_2 = AnalogPin.P8
+  export let SONAR = DigitalPin.P8
+  export let SERVO_1 = AnalogPin.P12
+  export let M2_PWR = AnalogPin.P13
+  export let M2_DIR = AnalogPin.P14
+  export let M1_PWR = AnalogPin.P15
+  export let M2_DIR = AnalogPin.P16
 }
