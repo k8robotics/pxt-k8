101e03d81200e3d492d8e288770514fbb9af5c4d
Luciano Abbott <luciano.abbott@gmail.com>
2018-06-12 11:00:37 -0400

    LA: Refactor block design and move advanced motion functions to advanced

--- motion.ts
+++ motion.ts
@@ -16,31 +16,48 @@ enum MotorDirection {
 namespace motion {
 
     /**
-    * Control the speed and direction of the left wheel
-    */
-    //% block
-    //% blockId=motion_left block="left wheel|speed: %speed|direction: %direction"
-    //% speed.min=0 speed.max=100
-    //% weight=82
-    export function setLeftWheel(speed: number, direction: MotorDirection): void {
-      if (direction == MotorDirection.REVERSE) {
-        speed *= -1
-      }
-      motorControl(Motor.LEFT, speed)
+     *Drives the robot straight at a specified speed
+     */
+     //% block
+     //% speed.min=-100 speed.max=100
+     //% weight=70
+    export function driveStraight(speed: number): void {
+        motorControl(Motor.LEFT, speed)
+        motorControl(Motor.RIGHT, speed)
     }
 
     /**
-    * Control the speed and direction of the right wheel
+     *Turns the robot to the left at a specified speed
+     */
+     //% block
+     //% speed.min=0 speed.max=100
+     //% weight=60
+    export function turnLeft(speed: number): void {
+      motorControl(Motor.LEFT, 0)
+      motorControl(Motor.RIGHT, speed)
+    }
+
+    /**
+     *Turns the robot to the right at a specified speed
+     */
+     //% block
+    //% speed.min=0 speed.max=100
+    //% weight=50
+    export function turnRight(speed: number): void {
+      motorControl(Motor.LEFT, speed)
+      motorControl(Motor.RIGHT, 0)
+    }
+
+    /**
+    * Control the speed and direction of a single wheel
     */
     //% block
-    //% blockId=motion_right block="right wheel|speed: %speed|direction: %direction"
+    //% blockId=motion_single block="drive |wheel: %wheel|speed: %speed"
     //% speed.min=0 speed.max=100
-    //% weight=81
-    export function setRightWheel(speed: number, direction: MotorDirection): void {
-      if (direction == MotorDirection.REVERSE) {
-        speed *= -1
-      }
-      motorControl(Motor.RIGHT, speed)
+    //% weight=49
+    //% advanced=true
+    export function driveWheel(wheel: Motor, speed: number): void {
+      motorControl(wheel, speed)
     }
 
     /**
@@ -52,51 +69,18 @@ namespace motion {
     //% blockId=motion_drive block="drive|left: %leftWheelSpeed|right: %rightWheelSpeed"
     //% leftWheelSpeed.min=-100 leftWheelSpeed.max=100
     //% rightWheelSpeed.min=-100 rightWheelSpeed.max=100
-    //% weight=80
+    //% weight=48
+    //% advanced=true
     export function drive(leftWheelSpeed: number, rightWheelSpeed: number): void {
         motorControl(Motor.LEFT, leftWheelSpeed)
         motorControl(Motor.RIGHT, rightWheelSpeed)
     }
 
-    /**
-     *Drives the robot straight at a specified speed
-     */
-     //% block
-     //% speed.min=-100 speed.max=100
-     //% weight = 70
-    export function driveStraight(speed: number): void {
-        motorControl(Motor.LEFT, speed)
-        motorControl(Motor.RIGHT, speed)
-    }
 
-    /**
-     *Turns the robot to the left at a specified speed
-     */
-     //% block
-     //% speed.min=-100 speed.max=100
-     //% weight = 60
-    export function turnLeft(speed: number): void {
-      motorControl(Motor.LEFT, 0)
-      motorControl(Motor.RIGHT, speed)
-    }
-
-    /**
-     *Turns the robot to the right at a specified speed
-     */
-     //% block
-    //% speed.min=-100 speed.max=100
-    //% weight = 50
-    export function turnRight(speed: number): void {
-      motorControl(Motor.LEFT, speed)
-      motorControl(Motor.RIGHT, 0)
-    }
 
     /**
      * Advanced control of an individual motor. PWM is set to constant value.
      */
-    //% block
-    //% speed.min=-100 speed.max=100
-    //% weight=80
     function motorControl(whichMotor: Motor, speed: number): void {
         let motorSpeed: number
         let direction: MotorDirection
