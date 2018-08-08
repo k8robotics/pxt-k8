231ab64ee0b0e276ffb7085c59312f884a19d23e
Luciano Abbott <luciano.abbott@gmail.com>
2018-08-08 13:45:58 -0400

    LA: Remove check all line sensors

--- lineSensors.ts
+++ lineSensors.ts
@@ -16,21 +16,6 @@ namespace lineSensors {
     WHITE
   }
 
-  /** 
-  * Check all sensors state and return a boolean array of the results.
-  * Indices: 0 is left, 1 is middle, 2 is right
-  */
-  //% block
-  //% weight=50
-  //% advanced=true
-  export function checkAllSensors(): Array<boolean> {
-    let result: Array<boolean>
-    result.push(pins.analogReadPin(k8.IR_SENSOR_LEFT) > 200)
-    result.push(pins.analogReadPin(k8.IR_SENSOR_MIDDLE) > 200)
-    result.push(pins.analogReadPin(k8.IR_SENSOR_RIGHT) > 200)
-    return result
-  }
-
   /**
    * Check if a chosen sensor is reading black or white
    * @param sensor which of the three sensors
